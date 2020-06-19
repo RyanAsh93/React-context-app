b935944a7d8e0f2242acc04c7eac142e5cf2ad03
RyanAsh93 <blueheadryan93@yahoo.com>
2020-06-19 15:41:19 -0600

    added some css

--- src/App.js
+++ src/App.js
@@ -4,19 +4,26 @@ import { Container } from 'semantic-ui-react';
 import { Switch, Route } from 'react-router-dom';
 import NavBar from './components/NavBar'
 import UserProfile from './components/UserProfile'
+import styled from 'styled-components'
 
 function App() {
   return (
-    <>
+    <AppContainer>
       <NavBar />
       <Container>
         <Switch>
-          <Route exact path='/'render={() => <div>Home</div>} />
+          <Route exact path='/'render={() => <h1>Welcome to the Home</h1>} />
           <Route exact path='/user/profile'render={() => <UserProfile />} />
         </Switch>
       </Container>
-    </>
+    </AppContainer>
   );
 }
+
+const AppContainer = styled.div`
+background: linear-gradient(to bottom right, #003973, #e5e5be);
+min-height: 100vh;
+padding: 20px;
+`;
 
 export default App;
